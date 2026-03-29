f6c1b3d26925702b709b5798a44917841f79386f
toffly <toffly@outlook.com>
2026-03-29 15:15:38 +0700

    setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fied top-0 z-50 md:p-9 p-3'>
+        <img src="/images/nav-logo.svg" alt="logo" className='md:w-24 w-20' />
+    </div>
+  )
+}
+
+export default Navbar
