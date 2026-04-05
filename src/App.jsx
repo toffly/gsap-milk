7eedb2e45c003028501637c01de61930747a1c16
toffly <toffly@outlook.com>
2026-04-05 09:24:00 +0700

    implements message section with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import Navbar from "./components/Navbar"
 import HeroSection from "./sections/HeroSection"
-import { ScrollTrigger, SplitText } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import MessageSection from "./sections/MessageSection";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText,ScrollSmoother)
+
+ScrollSmoother.create({
+	smooth: 1,
+	effects: true
+});
 
 const App = () => {
   return (
-    <main>
+    <main id="main-container">
       <Navbar/>
       <HeroSection/>
-      <div className="h-dvh bg-red-500"/>
+      <MessageSection/>
     </main>
   )
 }
